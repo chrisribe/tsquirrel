80212e8ec6007a06132291a4d4bf69ebb3da080a
chris <christian.ribe@gmail.com>
2024-05-09 23:04:21 -0400

    rename to api this is not frontend

--- start-demo.sh
+++ start-demo.sh
@@ -17,4 +17,9 @@ fi
 export DB_PASSWORD
 
 # Run docker-compose up
-docker-compose up
+# Check if the first argument is 'build'
+if [ "$1" == "build" ]; then
+  docker-compose up --build
+else
+  docker-compose up
+fi
