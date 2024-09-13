d8ffbabd75f861fd051aa89b56425810ef7ce2f1
chris <christian.ribe@gmail.com>
2024-09-12 23:55:52 -0400

    added login and more experiments

--- web/astro/astro.config.mjs
+++ web/astro/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
