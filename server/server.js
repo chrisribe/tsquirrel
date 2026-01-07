f886a45bf350646781933e86f1899a6f743a89bc
chris <christian.ribe@gmail.com>
2026-01-06 23:08:36 -0500

    feat: update asset version and enhance auth form styling for improved user experience

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 // Asset version for cache busting - increment on CSS/JS changes
-const ASSET_VERSION = '1.0.8';
+const ASSET_VERSION = '1.0.9';
 
 async function startServer() {
   const app = express();
